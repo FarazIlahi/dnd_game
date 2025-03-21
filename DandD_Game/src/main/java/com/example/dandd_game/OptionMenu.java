818a6a643bf02802d592799f853fde98b8ba455d
Faraz <farazahmedilahi@gmail.com>
2025-03-20 23:47:20 -0400

    Created Scenes and controller for GameLoads and OptionMenu. Still need to add funcationality for them.

---
+++ DandD_Game/src/main/java/com/example/dandd_game/OptionMenu.java
@@ -0,0 +1,15 @@
+package com.example.dandd_game;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.text.Text;
+
+public class OptionMenu {
+    @FXML
+    Slider audioSlider;
+    @FXML
+    Text audioNum;
+
+
+
+}
